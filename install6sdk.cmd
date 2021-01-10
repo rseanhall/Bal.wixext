1dffa6209ec9ca9decc6a77ec69a0d91fa6d0458
Sean Hall <r.sean.hall@gmail.com>
2021-01-10 14:27:29 -0600

    remove single quotes from install6sdk.cmd

--- install6sdk.cmd
+++ install6sdk.cmd
@@ -1,8 +1,2 @@
-@echo off
-PUSHD %~dp0
-
-appveyor DownloadFile 'https://dotnetcli.azureedge.net/dotnet/Sdk/6.0.100-alpha.1.21059.3/dotnet-sdk-6.0.100-alpha.1.21059.3-win-x64.exe' -FileName 'dotnet-6-sdk-win-x64.exe'
+appveyor DownloadFile https://dotnetcli.azureedge.net/dotnet/Sdk/6.0.100-alpha.1.21059.3/dotnet-sdk-6.0.100-alpha.1.21059.3-win-x64.exe -FileName dotnet-6-sdk-win-x64.exe
 dotnet-6-sdk-win-x64.exe /install /quiet
-
-POPD
-GOTO :EOF
