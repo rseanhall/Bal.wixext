993b45c166c5bf0dafb0aa8db8884402d9b5aed2
Sean Hall <r.sean.hall@gmail.com>
2021-01-10 14:35:21 -0600

    add messages to troubleshoot

--- install6sdk.cmd
+++ install6sdk.cmd
@@ -1,2 +1,3 @@
+appveyor AddMessage install6sdk
 appveyor DownloadFile https://dotnetcli.azureedge.net/dotnet/Sdk/6.0.100-alpha.1.21059.3/dotnet-sdk-6.0.100-alpha.1.21059.3-win-x64.exe -FileName dotnet-6-sdk-win-x64.exe
 dotnet-6-sdk-win-x64.exe /install /quiet
